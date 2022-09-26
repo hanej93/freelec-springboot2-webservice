c063fa05cfc9b451eaaf8b65fc0b7b5c12eb6721
EJ <hanej93@naver.com>
2022-09-26 22:50:39 +0900

    게시글 수정 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,8 +1,13 @@
 var main = {
     init : function () {
         var _this = this;
+
         $('#btn-save').on('click', function() {
            _this.save();
+        });
+
+        $('#btn-update').on('click', function() {
+           _this.update();
         });
     },
     save : function () {
@@ -23,6 +28,27 @@ var main = {
             window.location.href = '/';
         }).fail(function(error) {
             alert(JSON.stringify(error));
+        });
+    },
+    update: function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        }
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.strigify(error));
         });
     }
 };
